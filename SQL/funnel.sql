edf9e31cf0771a5aba7d1f2c222ab3358046f176
Shaivi <shaivisri04@gmail.com>
2025-12-16 17:12:35 +0530

    Update funnel.sql

--- SQL/funnel.sql
+++ SQL/funnel.sql
@@ -1,1 +1,16 @@
-this contains bigquery sql files
+SELECT
+s.session_id,
+s.customer_id,
+s.device,
+s.traffic_source,
+
+IFNULL(v.product_views,0) AS product_views,
+IFNULL(a.add_to_cart_events,0) AS add_to_cart_events,
+
+IF(v.product_views>0,1,0) AS view_flag,
+IF(a.add_to_cart_events>0,1,0) AS atc_flag,
+
+IFNULL(p.purchase_amount,0) AS purchase_amount,
+IF(p.purchase_amount>0,1,0) AS purchase_flag
+
+FROM `level-footing-481113-s9.ecommerce_funnel.funnel_base` AS s 
